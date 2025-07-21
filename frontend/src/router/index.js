d410c2afa3195acd809c84d138ad50b676f5756f
Lee-gaeun <kmeans203@naver.com>
2025-07-21 17:28:53 +0900

    feat: 용어집 ui 구현

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,7 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import HomePage from '@/pages/HomePage.vue'
+import TermView from "@/pages/TermView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [{ path: '/', name: 'home', component: HomePage }],
+  routes: [{ path: '/', name: 'home', component: HomePage },
+    {path: '/terms', component: TermView}
+  ],
 });
 export default router;
